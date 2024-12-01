829e6cd5d1cf55a77ee2b8900887012e8f4ceedc
Özge Kurnaz <ozge.k60@hotmail.com>
2024-12-01 23:31:41 +0300

    Initial Commit

---
+++ GuesstheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuesstheFlag
+//
+//  Created by özge kurnaz on 1.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
