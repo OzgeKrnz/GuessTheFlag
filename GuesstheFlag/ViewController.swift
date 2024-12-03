ec6955c296b262d3ac13956c42d9a9bf8127bcc4
Özge Kurnaz <ozge.k60@hotmail.com>
2024-12-03 21:50:14 +0300

    Auto Layout düzenlemeleri yapıldı.

--- GuesstheFlag/ViewController.swift
+++ GuesstheFlag/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia","france","germany","ireland","italy","monaco","nigeria","poland","russia", "spain","uk","us"]
+        
+        askQuestion()
     }
+    
+// write a method that shows three random flag imagex on the screen. Buttons have setImage() method that lets us control what picture is shown inside and when sho we can use that with UIImage to display our flag
+    func askQuestion(){
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+      
 
+
+    }
 
 }
 
